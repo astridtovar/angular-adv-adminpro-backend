6f1690ff42ac4e4c9e7c9415fe71f40d6dd99338
Astrid Tovar <astridtovar01@gmail.com>
2021-01-27 20:09:21 -0500

    Fin seccion 10 -Auth

--- index.js
+++ index.js
@@ -9,16 +9,15 @@ const app = express();
 // Configuracion cors
 app.use(cors());
 
+// Lectura y parseo Body
+app.use(express.json());
+
 // Base datos
 dbConnection();
 
 // Rutas
-app.get("/", (req, res) => {
-  res.status(400).json({
-    ok: true,
-    msg: "Hola mundo",
-  });
-});
+app.use("/api/usuarios", require("./routes/usuarios"));
+app.use("/api/login", require("./routes/auth"));
 
 app.listen(process.env.PORT, () => {
   console.log("Servidor corriendo en el puerto: " + process.env.PORT);
